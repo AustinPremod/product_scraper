d379941540e820b72fda5b0678f1b200b65650af
Austin Premod <premodaustin@gmail.com>
2024-05-10 18:49:28 +0530

    active admin

--- app/admin/products.rb
+++ app/admin/products.rb
@@ -7,4 +7,8 @@ ActiveAdmin.register Product do
   #
   permit_params :title, :description, :price, :contact_info, :author, :category, :scraped_at
   
+  filter :title
+  filter :contact_info
+  filter :author
+  filter :scraped_at
 end
